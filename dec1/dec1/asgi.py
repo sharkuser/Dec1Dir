74dbfd9dbc1f479185e02cffdd5a67b8583a1c21
maciek <maciej.karbowski@gmail.com>
2020-08-24 20:44:24 +0200

    hiding settings

---
+++ dec1/dec1/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for dec1 project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dec1.settings')
+
+application = get_asgi_application()
