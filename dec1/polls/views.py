65b2aa19c7efb2612968ff271f4aa7583e039cf5
maciek <maciej.karbowski@gmail.com>
2020-08-29 14:08:57 +0200

    views and urls

--- dec1/polls/views.py
+++ dec1/polls/views.py
@@ -4,18 +4,24 @@ from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
 from .models import Question
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
+    #template = loader.get_template('polls/index.html')
     context = {'latest_question_list' : latest_question_list}
     return render(request, 'polls/index.html', context)
     #render is a shortcut for HttpResponse with template:
     #return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")    
+    # return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
